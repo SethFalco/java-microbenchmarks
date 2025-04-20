0e325051eae8524c0f268cec0c4d24cc9e8ed948
Seth Falco <seth@falco.fun>
2025-04-20 13:34:40 +0100

    docs: add context for matchhexprefix

--- src/main/java/fun/falco/microbenchmarks/MatchHexPrefix.java
+++ src/main/java/fun/falco/microbenchmarks/MatchHexPrefix.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 /**
  * Check if the first 2 characters in a String indicate that the string
  * represents a hexadecimal number.
+ *
+ * @see <a href="https://github.com/apache/commons-beanutils/pull/49">apache/commons-beanutils#49</a>
  */
 public class MatchHexPrefix {
 
@@ -13,6 +15,9 @@ public class MatchHexPrefix {
     /** For {@link #regex}. */
     private static final Pattern HEX_PATTERN = Pattern.compile("^" + HEX_PREFIX, Pattern.CASE_INSENSITIVE);
 
+    /**
+     * @see <a href="https://github.com/apache/commons-beanutils/pull/49">Comment in apache/commons-beanutils#49.</a>
+     */
     public static boolean prefixEquals(final String value) {
         return value.substring(0, 2).equalsIgnoreCase(HEX_PREFIX);
     }
